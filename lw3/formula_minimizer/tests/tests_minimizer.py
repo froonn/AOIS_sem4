e32c31d70652e9f77115cc7d0cb7844b8d1e9b3a
froonn <137333195+froonn@users.noreply.github.com>
2025-05-31 09:56:57 +0300

    add tests lw3

--- lw3/formula_minimizer/tests/tests_minimizer.py
+++ lw3/formula_minimizer/tests/tests_minimizer.py
@@ -5,35 +5,46 @@ from formula_minimizer.formula_minimizer.minimizer import Formula
 class TestCalculativeMethod(unittest.TestCase):
     def test_calculative_method(self):
         formula = Formula('!A|B&C')
-        self.assertEqual(formula.calculated_method_cnf(), '(A|!C) & (A|!B)')
-        self.assertEqual(formula.calculated_method_dnf(), '(!B&!C) | (A)')
+        self.assertEqual(formula.calculated_method_cnf(), '(!A | B) & (!A | C)')
+        self.assertEqual(formula.calculated_method_dnf(), '(!A) | (B & C)')
 
         formula = Formula('(!A)&(B|C)')
+        self.assertEqual(formula.calculated_method_cnf(), '(!A) & (B | C)')
+        self.assertEqual(formula.calculated_method_dnf(), '(!A & C) | (!A & B)')
 
-        self.assertEqual(formula.calculated_method_cnf(), '(!B|!C) & (A)')
-        self.assertEqual(formula.calculated_method_dnf(), '(A&!C) | (A&!B)')
+        formula = Formula('(A->B)&(!C)&(D|E)')
+        self.assertEqual(formula.calculated_method_cnf(), '(!C) & (D | E) & (!A | B)')
+        self.assertEqual(formula.calculated_method_dnf(), '(!A & !C & E) | (B & !C & D) | (!A & !C & D) | (B & !C & E)')
 
 
 class TestTabularMethod(unittest.TestCase):
     def test_tabular_method(self):
         formula = Formula('!A|B&C')
-        self.assertEqual(formula.tabular_method_cnf(), '(A|!C) & (A|!B)')
-        self.assertEqual(formula.tabular_method_dnf(), '(!B&!C) | (A)')
+        self.assertEqual(formula.tabular_method_cnf(), '(!A | B) & (!A | C)')
+        self.assertEqual(formula.tabular_method_dnf(), '(!A) | (B & C)')
 
         formula = Formula('!A&(B|C)')
-        self.assertEqual(formula.tabular_method_cnf(), '(!B|!C) & (A)')
-        self.assertEqual(formula.tabular_method_dnf(), '(A&!C) | (A&!B)')
+        self.assertEqual(formula.tabular_method_cnf(), '(!A) & (B | C)')
+        self.assertEqual(formula.tabular_method_dnf(), '(!A & C) | (!A & B)')
+
+        formula = Formula('(A->B)&(!C)&(D|E)')
+        self.assertEqual(formula.tabular_method_cnf(), '(!C) & (D | E) & (!A | B)')
+        self.assertEqual(formula.tabular_method_dnf(), '(!A & !C & E) | (B & !C & D) | (!A & !C & D) | (B & !C & E)')
 
 
 class TestKmap(unittest.TestCase):
     def test_calculative_method(self):
         formula = Formula('!A|B&C')
-        self.assertEqual(' | '.join(formula.kmap_method_dnf()), '(!A&B&!C) | (!A&!B&!C) | (!A&!B&C) | (A&B&C) | (!A&B&C)')
-        self.assertEqual(' & '.join(formula.kmap_method_cnf()), '(!A|B|C) & (!A|B|!C) & (!A|!B|C)')
+        self.assertEqual(formula.kmap_method_cnf(), '(!A | B) & (!A | C)')
+        self.assertEqual(formula.kmap_method_dnf(), '(!A) | (B & C)')
 
         formula = Formula('!A&(B|C)')
-        self.assertEqual(' & '.join(formula.kmap_method_cnf()), '(!A|B|!C) & (!A|!B|C) & (A|B|C) & (!A|B|C) & (!A|!B|!C)')
-        self.assertEqual(' | '.join(formula.kmap_method_dnf()), '(!A&!B&C) | (!A&B&!C) | (!A&B&C)')
+        self.assertEqual(formula.kmap_method_cnf(), '(!A) & (B | C)')
+        self.assertEqual(formula.kmap_method_dnf(), '(!A & C) | (!A & B)')
+
+        formula = Formula('(A->B)&(!C)&(D|E)')
+        self.assertEqual(formula.kmap_method_cnf(), '(!C) & (D | E) & (!A | B)')
+        self.assertEqual(formula.kmap_method_dnf(), '(!A & !C & E) | (B & !C & D) | (!A & !C & D) | (B & !C & E)')
 
 
 if __name__ == '__main__':
